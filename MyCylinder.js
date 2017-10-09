90e8aa9f640cb0a10900e399a3e144103241c7a6
nunocr <nunocr963@gmail.com>
2017-10-09 17:28:27 +0100

    some changes on textures size

--- MyCylinder.js
+++ MyCylinder.js
@@ -12,6 +12,8 @@
 	this.topRad = parseFloat(args[2]);
 	this.stacks = parseFloat(args[3]);
 	this.slices = parseFloat(args[4]);
+	this.topCap = parseFloat(args[5]);
+	this.botCap = parseFloat(args[6]);
 	this.heightInc = this.height/this.stacks;
 
 	this.cylinder = new MyCylinderAux(this.scene, this.height, this.botRad, this.topRad, this.stacks, this.slices);
@@ -29,16 +31,22 @@
 		this.cylinder.display();
  	this.scene.popMatrix();
 
- 	this.scene.pushMatrix();
+
+	if(this.topCap == 1){
+		this.scene.pushMatrix();
  		this.scene.translate(0, 0, this.height);
  		this.scene.scale(this.topRad, this.topRad, 1);
 		this.top.display();
- 	this.scene.popMatrix();
+ 		this.scene.popMatrix();
+	}
 
- 	this.scene.pushMatrix();
+	if(this.botCap == 1){
+		this.scene.pushMatrix();
  		this.scene.rotate(Math.PI, 0, 1, 0);
  		this.scene.scale(this.botRad, this.botRad, 1);
 		this.bot.display();
- 	this.scene.popMatrix();
+ 		this.scene.popMatrix();
+	}
+
 
  };
