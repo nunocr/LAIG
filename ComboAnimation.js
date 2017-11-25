43915589929c8604dd8e01b349160a0662a32065
chaotixkilla <sergiodias.17@hotmail.com>
2017-11-25 19:16:52 +0000

    meh

--- ComboAnimation.js
+++ ComboAnimation.js
@@ -2,5 +2,12 @@ class ComboAnimation extends Animation{
     constructor(scene, id, type, animationRefs){
         super(scene, id, type);
         this.animationRefs = animationRefs;
+        this.animationTimes = [];
+
+        var timeAux = 0;
+        for(var i = 0; i < animationRefs.length; i++){
+            timeAux += this.scene.graph.animations[animationRefs[i]].getAnimationSpan();
+            this.animationTimes.push(timeAux);
+        }
     }
 }
