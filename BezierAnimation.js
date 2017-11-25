777fdabbd334075726601276d8731c4f34c0101d
nunocr <nunocr963@gmail.com>
2017-11-25 03:24:51 +0000

    hand animation added idk

--- BezierAnimation.js
+++ BezierAnimation.js
@@ -2,5 +2,9 @@ class BezierAnimation extends Animation{
     constructor(scene, id, speed, controlPoints){
         super(scene, id, speed);
         this.controlPoints = controlPoints;
+        this.animationMatrix = mat4.create();
+
+        this.sectionValues = [];
+        this.finalDistance = 0;
     }
 }
